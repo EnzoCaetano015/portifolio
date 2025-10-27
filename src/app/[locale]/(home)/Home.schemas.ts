913969c80000e70662a6c2c1edac42ddf508db83
EnzoCaetano015 <peracioenzo@gmail.com>
2025-10-27 17:47:17 -0300

    ajuste no schema

--- src/app/[locale]/(home)/Home.schemas.ts
+++ src/app/[locale]/(home)/Home.schemas.ts
@@ -1,10 +1,12 @@
 import * as Z from "zod"
 
 export const formSchema = Z.object({
-    name: Z.string({error: "Name is required"}),
-    email: Z.string({error: "Email is required"}).email("Invalid email address"),
-    subject: Z.string({error: "Subject is required"}),
-    message: Z.string({error: "Message is required"}),
+    name: Z.string({ error: "Name is required" }).min(2, "Name is required"),
+    email: Z.string({ error: "Email is required" })
+        .min(2, "Email is required")
+        .email("Invalid email address"),
+    subject: Z.string({ error: "Subject is required" }).min(2, "Subject is required"),
+    message: Z.string({ error: "Message is required" }).min(5, "Message is required"),
 })
 
 export type FormSchemaType = Z.infer<typeof formSchema>
